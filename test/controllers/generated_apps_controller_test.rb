0d35e3456d61c2444614633d9086b2d3e7e0ffab
Trinity Takei <trinity.takei@gmail.com>
2024-12-16 05:16:40 +0100

    100% coverage for GeneratedAppsController

--- test/controllers/generated_apps_controller_test.rb
+++ test/controllers/generated_apps_controller_test.rb
@@ -1,26 +1,86 @@
 require "test_helper"
+require_relative "../support/git_test_helper"
 
 class GeneratedAppsControllerTest < ActionDispatch::IntegrationTest
+  include GitTestHelper
+
   setup do
     @user = users(:jane)
-    @generated_app = generated_apps(:blog_app)
-    sign_in @user
+    sign_in(@user)
   end
 
   test "should show generated app" do
-    get generated_app_url(@generated_app)
+    get generated_app_url(generated_apps(:blog_app))
     assert_response :success
-
-    assert_select "h1", @generated_app.name
-    assert_select "p", @generated_app.description
-    assert_select "p", @generated_app.ruby_version
-    assert_select "p", @generated_app.rails_version
-    assert_select "a[href=?]", @generated_app.github_repo_url
   end
 
-  test "should not show generated app for unauthorized user" do
-    sign_out @user
-    get generated_app_url(@generated_app)
-    assert_redirected_to root_url
+  test "requires authentication" do
+    sign_out(@user)
+    post generated_apps_path, params: { app_name: "test-app" }
+    assert_redirected_to root_path
+    assert_equal "Please sign in first.", flash[:alert]
+  end
+
+  test "creates app with valid parameters" do
+    app_name = "my-test-app"
+    api_flag = "--api"
+    database = "--database=mysql"
+
+    Recipe.any_instance.stubs(:commit_changes).returns(true)
+    Recipe.any_instance.stubs(:initial_git_commit).returns(true)
+    GeneratedApp.any_instance.stubs(:commit_changes).returns(true)
+    GeneratedApp.any_instance.stubs(:initial_git_commit).returns(true)
+
+    assert_difference "GeneratedApp.count" do
+      assert_difference "Recipe.count" do
+        post generated_apps_path, params: {
+          app_name: app_name,
+          api_flag: api_flag,
+          database_choice: database
+        }
+      end
+    end
+
+    app = GeneratedApp.last
+    assert_equal app_name, app.name
+    assert_equal @user, app.user
+    assert_equal "#{api_flag} #{database}", app.recipe.cli_flags
+
+    assert_redirected_to generated_app_log_entries_path(app)
+  end
+
+  test "reuses existing recipe if cli flags match" do
+    recipe = recipes(:api_recipe) # Has "--api --database=postgresql" flags
+
+    GeneratedApp.any_instance.stubs(:commit_changes).returns(true)
+    GeneratedApp.any_instance.stubs(:initial_git_commit).returns(true)
+
+    assert_difference "GeneratedApp.count" do
+      assert_no_difference "Recipe.count" do
+        post generated_apps_path, params: {
+          app_name: "new-api",
+          api_flag: "--api",
+          database_choice: "--database=postgresql"
+        }
+      end
+    end
+
+    app = GeneratedApp.last
+    assert_equal recipe, app.recipe
+  end
+
+  test "starts app generation after creation" do
+    Recipe.any_instance.stubs(:commit_changes).returns(true)
+    Recipe.any_instance.stubs(:initial_git_commit).returns(true)
+    GeneratedApp.any_instance.stubs(:commit_changes).returns(true)
+    GeneratedApp.any_instance.stubs(:initial_git_commit).returns(true)
+
+    AppGeneration::Orchestrator.any_instance.expects(:call)
+
+    post generated_apps_path, params: {
+      app_name: "test-app",
+      api_flag: "--api",
+      database_choice: "--database=mysql"
+    }
   end
 end
