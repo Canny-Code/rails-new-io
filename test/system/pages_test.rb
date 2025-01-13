c11178ef71c13d8ceb9d0281fc8bac43f3821c20
Trinity Takei <trinity.takei@gmail.com>
2025-01-13 20:25:57 +0100

    Fix system tests

--- test/system/pages_test.rb
+++ test/system/pages_test.rb
@@ -1,46 +1,43 @@
 require "application_system_test_case"
 
 class PagesTest < ApplicationSystemTestCase
+  def setup
+    @user = users(:john)
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(
+      provider: @user.provider,
+      uid: @user.uid,
+      info: {
+        email: @user.email,
+        name: @user.name,
+        image: @user.image,
+        nickname: @user.github_username
+      }
+    )
+  end
+
   test "visiting the basic setup page" do
     visit setup_recipes_path(slug: pages(:basic_setup).slug)
 
-    assert_selector "h1", text: "Create a New Rails App"
+    assert_selector "h3", text: "Databases", wait: 5
     assert_equal setup_recipes_path(slug: pages(:basic_setup).slug), current_path
   end
 
   test "can navigate between pages" do
     visit setup_recipes_path(slug: pages(:basic_setup).slug)
 
-    click_on "Custom Ingredients"
+    click_on "Your Custom Ingredients"
 
-    assert_selector "h1", text: "Custom Ingredients"
+    assert_selector "h3", text: "Your Custom Ingredients", wait: 5
     assert_equal setup_recipes_path(slug: pages(:custom_ingredients).slug), current_path
   end
 
   test "can navigate between pages using tabs" do
     visit setup_recipes_path(slug: pages(:basic_setup).slug)
 
-    find("#custom-ingredients-tab").click
+    find("#your-custom-ingredients-tab").click
 
-    assert_selector "h1", text: "Custom Ingredients"
+    assert_selector "h3", text: "Your Custom Ingredients", wait: 5
     assert_equal setup_recipes_path(slug: pages(:custom_ingredients).slug), current_path
-  end
-
-  test "can navigate between pages using keyboard shortcuts" do
-    visit setup_recipes_path(slug: pages(:basic_setup).slug)
-
-    find("body").send_keys("2")
-
-    assert_selector "h1", text: "Custom Ingredients"
-    assert_equal setup_recipes_path(slug: pages(:custom_ingredients).slug), current_path
-  end
-
-  test "can navigate between pages using keyboard shortcuts in reverse" do
-    visit setup_recipes_path(slug: pages(:custom_ingredients).slug)
-
-    find("body").send_keys("1")
-
-    assert_selector "h1", text: "Basic Setup"
-    assert_equal setup_recipes_path(slug: pages(:basic_setup).slug), current_path
   end
 end
