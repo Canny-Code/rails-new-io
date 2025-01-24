1da5653242d3c0979e2f8d7acb090e4f85f9e861
Trinity Takei <trinity.takei@gmail.com>
2025-01-24 21:15:57 +0100

    Fix buffer_test.rb

--- test/services/command_execution_service/buffer_test.rb
+++ test/services/command_execution_service/buffer_test.rb
@@ -3,26 +3,26 @@ require "test_helper"
 class CommandExecutionService::BufferTest < ActiveSupport::TestCase
   def setup
     @user = users(:john)
-    @recipe = recipes(:blog)
+    @recipe = recipes(:blog_recipe)
     @app = GeneratedApp.create!(
       user: @user,
       recipe: @recipe,
       name: "test-app",
-      ruby_version: "3.2.2",
-      rails_version: "7.1.2",
       selected_gems: [],
       configuration_options: {}
     )
     @buffer = CommandExecutionService::Buffer.new(@app)
+    @initial_log_count = @app.log_entries.count
   end
 
   test "creates new log entry during flush if @log_entry is nil" do
+    @buffer.instance_variable_set(:@log_entry, nil) # Force creation of new entry
     @buffer.append("test output")
     @buffer.flush
 
-    assert_equal 1, @app.log_entries.count
-    assert_equal "test output", @app.log_entries.first.message
-    assert_equal "info", @app.log_entries.first.entry_type
+    assert_equal @initial_log_count + 1, @app.log_entries.count
+    assert_equal "Initializing Rails application generation...\ntest output", @app.log_entries.last.message
+    assert_equal "rails_output", @app.log_entries.last.entry_type
   end
 
   test "updates log entry message without changing entry_type when not completed" do
@@ -31,8 +31,8 @@ class CommandExecutionService::BufferTest < ActiveSupport::TestCase
     @buffer.append("second line")
     @buffer.flush
 
-    assert_equal 1, @app.log_entries.count
-    assert_equal "first line\nsecond line", @app.log_entries.first.message
-    assert_equal "info", @app.log_entries.first.entry_type
+    assert_equal @initial_log_count, @app.log_entries.count
+    assert_equal "Initializing Rails application generation...\nfirst line\nsecond line", @app.log_entries.last.message
+    assert_equal "rails_output", @app.log_entries.last.entry_type
   end
 end
