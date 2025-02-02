4dfe59573e033fe576fce08aca1dbb711623be1b
Trinity Takei <trinity.takei@gmail.com>
2025-02-02 10:24:19 +0100

    Bugfix: repository_service should be an ivar

--- app/models/generated_app.rb
+++ app/models/generated_app.rb
@@ -67,7 +67,7 @@ class GeneratedApp < ApplicationRecord
       apply_ingredient(ingredient)
 
       @logger.info("Committing ingredient changes")
-      repository_service.commit_changes_after_applying_ingredient(ingredient)
+      @repository_service.commit_changes_after_applying_ingredient(ingredient)
       @logger.info("Ingredient applied successfully", { name: ingredient.name })
     end
   end
