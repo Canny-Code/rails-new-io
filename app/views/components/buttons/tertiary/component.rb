d8028f2896ee442f87a699130ed09bc737959eec
Mia Sinek <mia@miasinek.com>
2025-02-12 20:24:28 +0100

    Fix tertiary button component

--- app/views/components/buttons/tertiary/component.rb
+++ app/views/components/buttons/tertiary/component.rb
@@ -47,7 +47,7 @@ module Buttons
       def button_classes
         base_classes = "inline-flex items-center rounded-md font-semibold transition ease-in-out duration-150"
         size_classes = @size == :large ? "px-8 py-4 text-lg" : "px-3 py-2 text-sm"
-        state_classes = "text-[#993351] hover:text-[#B34766] focus:underline focus:text-[#993351] active:text-[#731F39] disabled:text-[#D3A9B6] disabled:cursor-not-allowed"
+        state_classes = "text-[#993351] hover:text-[#B34766] hover:bg-gray-50 focus:underline focus:text-[#993351] active:text-[#731F39] disabled:text-[#D3A9B6] disabled:cursor-not-allowed"
         border_classes = "border border-[#993351] hover:border-[#B34766] active:border-[#731F39] disabled:border-[#D3A9B6]"
         [ base_classes, size_classes, state_classes, border_classes, @html_options[:class] ].reject(&:blank?).join(" ")
       end
