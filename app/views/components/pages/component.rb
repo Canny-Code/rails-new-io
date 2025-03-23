debb98f66a6018728589f288b31cae314c5921b3
Trinity Takei <trinity.takei@gmail.com>
2025-03-23 12:46:50 +0100

    First version of the onboarding sidebar

--- app/views/components/pages/component.rb
+++ app/views/components/pages/component.rb
@@ -4,30 +4,45 @@ module Pages
   class Component < ApplicationComponent
     include Phlex::Rails::Helpers::LinkTo
 
-    def initialize(page:)
+    def initialize(page:, onboarding_step: nil)
       @page = page
+      @onboarding_step = onboarding_step
     end
 
     def view_template
-      div(class: "max-w-4xl mx-auto py-8 space-y-8") do
-        if @page.groups.any?
-          div(class: "space-y-8") do
-            @page.groups.each do |group|
-              render Pages::Groups::Component.new(group: group)
+      if @page.groups.any?
+        if @onboarding_step.present?
+          div(class: "flex gap-0 max-w-6xl mx-auto") do
+            #  Left sidebar (1/3)
+            render "shared/onboarding_sidebar"
+
+            # Main content (2/3)
+            div(class: "space-y-8 w-2/3 ") do
+              @page.groups.each do |group|
+                render Pages::Groups::Component.new(group: group)
+              end
             end
           end
         else
-          div(class: "bg-white rounded-lg border border-gray-200") do
-            render EmptyState::Component.new(
-              user: Current.user,
-              title: "No ingredients yet",
-              description: "Get started by adding your first ingredient.",
-              button_text: "Add new Ingredient",
-              icon: true,
-              button_path: new_ingredient_path,
-              emoji: "🧂"
-            )
+          div(class: "max-w-4xl mx-auto py-8 space-y-8") do
+            div(class: "space-y-8") do
+              @page.groups.each do |group|
+                render Pages::Groups::Component.new(group: group)
+              end
+            end
           end
+        end
+      else
+        div(class: "bg-white rounded-lg border border-gray-200") do
+          render EmptyState::Component.new(
+            user: Current.user,
+            title: "No ingredients yet",
+            description: "Get started by adding your first ingredient.",
+            button_text: "Add new Ingredient",
+            icon: true,
+            button_path: new_ingredient_path,
+            emoji: "🧂"
+          )
         end
       end
     end
