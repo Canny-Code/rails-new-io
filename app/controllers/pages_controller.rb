4283a6973b40494ff3a6857ef820549c7019b515
Trinity Takei <trinity.takei@gmail.com>
2025-04-07 17:59:13 +0200

    Optimize query: only load groups/sub_groups/elements that belong to the current user/rails-new-io

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,17 +8,22 @@ class PagesController < ApplicationController
           elements: [ :variant, :user ]
         }
       }
-    ).friendly.find(params[:slug])
+    ).joins(groups: { sub_groups: { elements: :user } })
+     .where("elements.user_id = ? OR users.github_username = ?", Current.user.id, "rails-new-io")
+     .friendly.find(params[:slug])
   end
 
   def edit
     @page = Page.includes(
       groups: {
         sub_groups: {
-          elements: [ :variant ]
+          elements: [ :variant, :user ]
         }
       }
-    ).friendly.find(params[:slug])
+    ).joins(groups: { sub_groups: { elements: :user } })
+     .where("elements.user_id = ? OR users.github_username = ?", Current.user.id, "rails-new-io")
+     .friendly.find(params[:slug])
+
     @recipe = Recipe.find(params[:recipe_id])
   end
 end
