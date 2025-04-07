a931b4b177bd3afafff188e67be00782a82885a8
Trinity Takei <trinity.takei@gmail.com>
2025-04-07 18:54:25 +0200

    Massive simplification after cleaning up Ingredient:checkbox 1:1

--- app/services/ingredient_ui_destroyer.rb
+++ app/services/ingredient_ui_destroyer.rb
@@ -9,25 +9,22 @@ class IngredientUiDestroyer
 
   def call
     ActiveRecord::Base.transaction do
-      page = Page.find_by!(title: "Your Custom Ingredients")
+      checkbox = Element::CustomIngredientCheckbox.find_by(ingredient_id: ingredient.id)
+      return unless checkbox
 
-      group = page.groups.find_by(title: ingredient.category)
-      return unless group
-
-      sub_group = group.sub_groups.find_by(title: "Default")
-      return unless sub_group
-
-      element = sub_group.elements.find_by(label: ingredient.name)
+      element = checkbox.element
       return unless element
 
-      # Delete the element
+      sub_group = element.sub_group
+      group = sub_group.group
+
+      # Destroy the element (this will also destroy the checkbox due to delegated_type)
       element.destroy
 
-      # If this was the last element in the sub_group, delete the sub_group
+      # Clean up empty ancestors
       if sub_group.elements.reload.empty?
         sub_group.destroy
 
-        # If this was the last sub_group in the group, delete the group
         if group.sub_groups.reload.empty?
           group.destroy
         end
--- app/services/ingredient_ui_updater.rb
+++ app/services/ingredient_ui_updater.rb
@@ -5,91 +5,64 @@ class IngredientUiUpdater
 
   def initialize(ingredient)
     @ingredient = ingredient
-    @groups_to_check = Set.new
   end
 
   def call
-    page = Page.find_by(title: "Your Custom Ingredients")
-    return unless page
+    checkbox = ingredient.custom_ingredient_checkbox
+    return unless checkbox
 
-    elements_to_process = Element.joins(:sub_group)
-                    .joins("INNER JOIN groups ON groups.id = sub_groups.group_id")
-                    .joins("INNER JOIN element_custom_ingredient_checkboxes ON element_custom_ingredient_checkboxes.id = elements.variant_id")
-                    .where(groups: { page_id: page.id })
-                    .where(variant_type: "Element::CustomIngredientCheckbox")
-                    .where(element_custom_ingredient_checkboxes: { ingredient_id: ingredient.id })
-                    .includes(sub_group: :group)  # Eager load associations
-                    .to_a
-    return if elements_to_process.empty?
+    element = checkbox.element
+    return unless element
+
+    old_sub_group = element.sub_group
+    old_group = old_sub_group.group
 
     ActiveRecord::Base.transaction do
-      elements_to_process.each do |element|
-        new_label = if element.label =~ /\(.*\)/
-                     suffix = element.label.match(/\(.*\)/).to_s
-                     "#{ingredient.name} #{suffix}"
-        else
-                     ingredient.name
-        end
-
-        old_sub_group = element.sub_group
-        old_group = old_sub_group.group
-
-        needs_move = !old_group.title.eql?(ingredient.category)
-
-        if needs_move
-          move_element_to_new_group(element, old_group, old_sub_group, new_label, page)
-        else
-          handle_unchanged_category(element, new_label)
-        end
+      if old_group.title != ingredient.category
+        move_element_to_new_group(element, old_group, old_sub_group)
+      else
+        update_element_attributes(element)
       end
 
-      # Clean up empty groups and their sub_groups
+      # Clean up empty sub_groups and groups
+      if old_sub_group.elements.reload.empty?
+        old_sub_group.destroy!
 
-      groups_to_check.each do |group|
-        begin
-          group.reload
-          group.sub_groups.each do |sub_group|
-            if sub_group.elements.count.zero?
-              sub_group.destroy!
-            end
-          end
-          group.reload
-
-          group.destroy! if group.sub_groups.count.zero?
-        rescue ActiveRecord::RecordNotFound => e
-          next
-        end
+        old_group.reload
+        old_group.destroy! if old_group.sub_groups.empty?
       end
     end
   end
 
   private
 
-  def move_element_to_new_group(element, old_group, old_sub_group, new_label, page)
+  def move_element_to_new_group(element, old_group, old_sub_group)
+    page = old_group.page
     new_group = page.groups.find_or_create_by!(title: ingredient.category, behavior_type: "custom_ingredient_checkbox")
     new_sub_group = new_group.sub_groups.find_or_create_by!(title: old_sub_group.title)
 
-    # Track for cleanup
-    groups_to_check << old_group
-
-    # Move the element and update its attributes
     element.update!(
       sub_group: new_sub_group,
-      label: new_label,
+      label: new_label_with_suffix(element.label),
       description: ingredient.description
     )
   end
 
-  def handle_unchanged_category(element, new_label)
-    update_attributes_only(element, new_label)
-  end
-
-  def update_attributes_only(element, new_label)
+  def update_element_attributes(element)
     element.update!(
-      label: new_label,
+      label: new_label_with_suffix(element.label),
       description: ingredient.description
     )
   end
 
-  attr_reader :ingredient, :groups_to_check
+  def new_label_with_suffix(old_label)
+    if old_label =~ /\(.*\)/
+      suffix = old_label.match(/\(.*\)/).to_s
+      "#{ingredient.name} #{suffix}"
+    else
+      ingredient.name
+    end
+  end
+
+  attr_reader :ingredient
 end
