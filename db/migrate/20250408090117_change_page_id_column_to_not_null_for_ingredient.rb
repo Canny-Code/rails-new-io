230dd9463a54bfaf89bebae59f51c06790b2b77a
Trinity Takei <trinity.takei@gmail.com>
2025-04-17 19:54:08 +0200

    Delete orphaned (page_id=NULL) ingredients

--- db/migrate/20250408090117_change_page_id_column_to_not_null_for_ingredient.rb
+++ db/migrate/20250408090117_change_page_id_column_to_not_null_for_ingredient.rb
@@ -1,5 +1,9 @@
 class ChangePageIdColumnToNotNullForIngredient < ActiveRecord::Migration[8.0]
   def change
+    # First, delete any ingredients without a page_id
+    execute("DELETE FROM ingredients WHERE page_id IS NULL")
+
+    # Then add the NOT NULL constraint
     change_column_null :ingredients, :page_id, false
   end
 end
