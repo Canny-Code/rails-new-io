1b01f615f9b300ceb4998be843396ad8e1db5aa0
Trinity Takei <trinity.takei@gmail.com>
2025-04-17 20:34:16 +0200

    temp turn off foreign key integrity check

--- db/migrate/20250408090117_change_page_id_column_to_not_null_for_ingredient.rb
+++ db/migrate/20250408090117_change_page_id_column_to_not_null_for_ingredient.rb
@@ -1,15 +1,27 @@
 class ChangePageIdColumnToNotNullForIngredient < ActiveRecord::Migration[8.0]
   def change
-    # First remove the foreign key constraint
-    remove_foreign_key :ingredients, :pages
+    # Drop the table completely
+    drop_table :ingredients
 
-    # Then delete ingredients without page_id
-    execute("DELETE FROM ingredients WHERE page_id IS NULL")
+    # Recreate it with the correct constraints
+    create_table :ingredients do |t|
+      t.string :name, null: false
+      t.text :description
+      t.text :template_content, null: false
+      t.text :conflicts_with
+      t.text :requires
+      t.text :configures_with
+      t.integer :created_by_id, null: false
+      t.string :category
+      t.json :snippets, default: []
+      t.integer :page_id, null: false
+      t.string :sub_category, default: "Default"
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
+    end
 
-    # Add the NOT NULL constraint
-    change_column_null :ingredients, :page_id, false
-
-    # Finally add back the foreign key constraint
-    add_foreign_key :ingredients, :pages
+    add_index :ingredients, :created_by_id
+    add_index :ingredients, [ :name, :created_by_id, :page_id, :category, :sub_category ], unique: true, name: "index_ingredients_on_name_scope"
+    add_index :ingredients, :page_id
   end
 end
