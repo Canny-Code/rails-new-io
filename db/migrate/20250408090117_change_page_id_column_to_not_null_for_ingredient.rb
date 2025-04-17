c8f8f7d26de73997f79d9504531da0de6928b4bb
Trinity Takei <trinity.takei@gmail.com>
2025-04-17 21:12:08 +0200

    temp turn off foreign key integrity check

--- db/migrate/20250408090117_change_page_id_column_to_not_null_for_ingredient.rb
+++ db/migrate/20250408090117_change_page_id_column_to_not_null_for_ingredient.rb
@@ -1,9 +1,7 @@
 class ChangePageIdColumnToNotNullForIngredient < ActiveRecord::Migration[8.0]
   def change
-    # Defer foreign key checks until the end of the transaction
-    execute("PRAGMA defer_foreign_keys = ON")
-
-    # Just drop the table
-    drop_table :ingredients if table_exists?(:ingredients)
+    disable_referential_integrity do
+      drop_table :ingredients if table_exists?(:ingredients)
+    end
   end
 end
