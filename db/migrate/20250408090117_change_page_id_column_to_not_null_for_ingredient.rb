a5ba393d36b72ff85551511ac8df2d5c44811ce0
Trinity Takei <trinity.takei@gmail.com>
2025-04-17 20:24:09 +0200

    temp turn off foreign key integrity check

--- db/migrate/20250408090117_change_page_id_column_to_not_null_for_ingredient.rb
+++ db/migrate/20250408090117_change_page_id_column_to_not_null_for_ingredient.rb
@@ -1,12 +1,15 @@
 class ChangePageIdColumnToNotNullForIngredient < ActiveRecord::Migration[8.0]
   def change
-    # First delete any dependent records
-    execute("DELETE FROM element_custom_ingredient_checkboxes WHERE ingredient_id IN (SELECT id FROM ingredients WHERE page_id IS NULL)")
+    # First remove the foreign key constraint
+    remove_foreign_key :ingredients, :pages
 
     # Then delete ingredients without page_id
     execute("DELETE FROM ingredients WHERE page_id IS NULL")
 
-    # Finally add the NOT NULL constraint
+    # Add the NOT NULL constraint
     change_column_null :ingredients, :page_id, false
+
+    # Finally add back the foreign key constraint
+    add_foreign_key :ingredients, :pages
   end
 end
