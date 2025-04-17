829b2991704f42449a44da3ce7feca5bbe727fcd
Trinity Takei <trinity.takei@gmail.com>
2025-04-17 20:45:35 +0200

    temp turn off foreign key integrity check

--- db/migrate/20250408090117_change_page_id_column_to_not_null_for_ingredient.rb
+++ db/migrate/20250408090117_change_page_id_column_to_not_null_for_ingredient.rb
@@ -1,12 +1,11 @@
 class ChangePageIdColumnToNotNullForIngredient < ActiveRecord::Migration[8.0]
   def change
-    # First drop dependent tables
-    drop_table :element_custom_ingredient_checkboxes if table_exists?(:element_custom_ingredient_checkboxes)
+    # Disable foreign key checks
+    execute("PRAGMA foreign_keys = OFF")
 
-    # Then drop the ingredients table
+    # Drop and recreate the table
     drop_table :ingredients if table_exists?(:ingredients)
 
-    # Recreate ingredients with the correct constraints
     create_table :ingredients do |t|
       t.string :name, null: false
       t.text :description
@@ -27,16 +26,7 @@ class ChangePageIdColumnToNotNullForIngredient < ActiveRecord::Migration[8.0]
     add_index :ingredients, [ :name, :created_by_id, :page_id, :category, :sub_category ], unique: true, name: "index_ingredients_on_name_scope"
     add_index :ingredients, :page_id
 
-    # Recreate dependent tables
-    create_table :element_custom_ingredient_checkboxes do |t|
-      t.boolean :checked
-      t.boolean :default
-      t.integer :ingredient_id, null: false
-      t.datetime :created_at, null: false
-      t.datetime :updated_at, null: false
-    end
-
-    add_index :element_custom_ingredient_checkboxes, :ingredient_id
-    add_index :element_custom_ingredient_checkboxes, :ingredient_id, unique: true, name: "index_element_custom_ingredient_checkboxes_unique_ingredient"
+    # Re-enable foreign key checks
+    execute("PRAGMA foreign_keys = ON")
   end
 end
